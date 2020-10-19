2bb730924729ccbde9497d8c9a6def10a0a7a5d4
cesarvin <vinizio007@gmail.com>
2020-10-18 19:48:48 -0600

    avances

--- proyecto.py
+++ proyecto.py
@@ -3,15 +3,15 @@ from obj import Obj, Texture, Envmap
 from sphere import *
 import random
 
-wall = Material(diffuse = color( 0.49, 0.67, 0.48 ), spec = 16)
-roof = Material(diffuse= color( 0.66, 0.84, 0.67 ), spec = 16)
-floor = Material(diffuse = color(0.4, 0.35, 0.35 ), spec = 16)
-cubo = Material(diffuse = color(0.4, 0.69, 0.8 ), spec = 32)
+basurero = Material(diffuse = color( 0.49, 0.67, 0.48 ), spec = 16)
+cubo1 = Material(diffuse= color( 0.66, 0.84, 0.67 ), spec = 16)
+cubo2 = Material(diffuse = color(0.4, 0.35, 0.35 ), spec = 16)
+cubo3 = Material(diffuse = color(0.4, 0.69, 0.8 ), spec = 32)
 reflexion = Material(spec = 64, matType = REFLECTIVE)
 refraxion = Material(spec = 64, ior = 2.5, matType= TRANSPARENT) 
 
-width = 1000
-height = 500
+width = 500
+height = 250
 r = Raytracer(width,height)
 r.glClearColor(0.2, 0.6, 0.8)
 r.glClear()
@@ -38,9 +38,17 @@ r.scene.append( AABB(V3(7.7, 10, -25), V3(0.05, 15, 4.5) , refraxion ) )
 r.scene.append( AABB(V3(8.1, 10, -18), V3(0.05, 15, 5.1) , refraxion ) )
 r.scene.append( AABB(V3(8.5, 9.9, -13), V3(0.05, 15, 3.9) , refraxion ) )
 
-# r.scene.append( Sphere(V3(-1.5, -1, -10), 2, reflexion) )
-# r.scene.append( Sphere(V3(1.5, -1, -10), 2, refraxion) )
+# Esferas 
+r.scene.append( Sphere(V3(10, -6, -30), 3, reflexion) )
+r.scene.append( Sphere(V3(10, -6, -15), 3, refraxion) )
 
+# cajas
+r.scene.append( AABB(V3(0, -2.1, -10), V3(1, 1, 1), cubo1 ) )
+r.scene.append( AABB(V3(0, -2.1, -15), V3(1, 1, 1), cubo2 ) )
+r.scene.append( AABB(V3(0, -2.1, -20), V3(1, 1, 1), cubo3 ) )
+r.scene.append( AABB(V3(0, -2.1, -25), V3(1, 1, 1), cubo1 ) )
+r.scene.append( AABB(V3(0, -2.1, -30), V3(1, 1, 1), cubo2 ) )
+r.scene.append( AABB(V3(0, -2.1, -35), V3(1, 1, 1), cubo3 ) )
 
 r.rtRender()
 
